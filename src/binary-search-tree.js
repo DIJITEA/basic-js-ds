7990200ac5d306970aced1fe61503000f9b06254
Oleg <Kornyakovo@gmail.com>
2021-10-19 01:54:16 +0400

    binary-search-tree root/add/min/max

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,66 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 module.exports = class BinarySearchTree {
+  constructor() {
+    this.treeRoot = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data ) {
+    let node = new Node(data);
+    let temp = this.treeRoot;
+    if (!this.treeRoot) {
+      return (this.treeRoot = node);
+    }
+    while (temp) {
+      if (node.data < temp.data) {
+        if (!temp.left) {
+          return (temp.left = node);
+        }
+        temp = temp.left;
+      } else {
+        if (!temp.right) {
+          return (temp.right = node);
+        }
+        temp = temp.right;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    throw new NotImplementedError("Not implemented");
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    throw new NotImplementedError("Not implemented");
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(/*data*/) {
+    throw new NotImplementedError("Not implemented");
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let goLeft = this.treeRoot;
+    while (goLeft.left) {
+      goLeft = goLeft.left;
+    }
+    return goLeft.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let goRight = this.treeRoot;
+    while (goRight.right) {
+      goRight = goRight.right;
+    }
+    return goRight.data;
   }
-
-}
+};
